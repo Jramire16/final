1b4ab996026e2788937c8d552906a64ceb8f7e65
James Ramirez <jramire678@gmail.com>
2020-05-24 14:25:49 -0400

    Updates

---
+++ precache-manifest.c3416c9d2b5e5004e23588ec96b33d03.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a5193eba89ad8291ec60f718d3b907a",
+    "url": "/final/index.html"
+  },
+  {
+    "revision": "0307a968224c6ac65eff",
+    "url": "/final/static/css/2.05a0ae32.chunk.css"
+  },
+  {
+    "revision": "8b1d9f0284779cd0da1d",
+    "url": "/final/static/css/main.c839ff79.chunk.css"
+  },
+  {
+    "revision": "0307a968224c6ac65eff",
+    "url": "/final/static/js/2.179393a2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/final/static/js/2.179393a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b1d9f0284779cd0da1d",
+    "url": "/final/static/js/main.3c6ff0c3.chunk.js"
+  },
+  {
+    "revision": "2cf6a6a84dab828ac3b5",
+    "url": "/final/static/js/runtime-main.90441373.js"
+  },
+  {
+    "revision": "67ecdbfea5756e0f7079634a75cd2f91",
+    "url": "/final/static/media/cheesepizza.67ecdbfe.jpg"
+  },
+  {
+    "revision": "39077f4824498848a4ceda04f8ddf732",
+    "url": "/final/static/media/hawaiianpizza.39077f48.jpg"
+  },
+  {
+    "revision": "53e76c1801a60de19106caab943ddf96",
+    "url": "/final/static/media/pepperonipizza.53e76c18.jpg"
+  },
+  {
+    "revision": "65fa5a85e5607eefcad8893d05cedafc",
+    "url": "/final/static/media/sausagepizza.65fa5a85.jpg"
+  }
+]);
